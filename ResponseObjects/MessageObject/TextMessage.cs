0de5033bd1679e3ef96706d274ed9654a5d5950e
q55mehqs <s1701057@sendai-nct.jp>
2020-05-14 18:01:36 +0900

    add complete kadai

--- ResponseObjects/MessageObject/TextMessage.cs
+++ ResponseObjects/MessageObject/TextMessage.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace LineApi.ResponseObjects.MessageObject
@@ -6,10 +7,25 @@ namespace LineApi.ResponseObjects.MessageObject
     {
         [JsonProperty(PropertyName = "type")] public string Type => "text";
         [JsonProperty(PropertyName = "text")] public string Text { get; }
+        [JsonProperty(PropertyName = "emojis")] public List<Emoji>? Emojis { get; set; }
 
         public TextMessage(string text)
         {
             Text = text;
+        }
+    }
+
+    public class Emoji
+    {
+        [JsonProperty(PropertyName = "index")] public int Index { get; }
+        [JsonProperty(PropertyName = "productId")] public string ProductId { get; }
+        [JsonProperty(PropertyName = "emojiId")] public string EmojiId { get; }
+
+        public Emoji(int index, string productId, string emojiId)
+        {
+            Index = index;
+            ProductId = productId;
+            EmojiId = emojiId;
         }
     }
 }
