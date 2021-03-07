7ec69739812cc4c9640d399b4b118f1c8b518b61
q55mehqs <s1701057@sendai-nct.jp>
2021-03-07 10:40:40 +0900

    move ClassListColumns to ClassColumn from ColumnObject

--- ResponseObjects/MessageObject/TemplateObject/ColumnObject.cs
+++ ResponseObjects/MessageObject/TemplateObject/ColumnObject.cs
@@ -33,41 +33,5 @@ namespace LineApi.ResponseObjects.MessageObject.TemplateObject
             Text = text;
             Actions = actions;
         }
-
-        public static IEnumerable<ColumnObject> ClassListColumns(string command, bool useAs = true,
-            string asColumnText = "応用科学コース", IActionObject? asFooter = null)
-        {
-            var columns = new List<ColumnObject>();
-            var grade1Actions = new[]
-            {
-                new MessageAction("1-1", $">> {command} 1-1"),
-                new MessageAction("1-2", $">> {command} 1-2"),
-                new MessageAction("1-3", $">> {command} 1-3")
-            };
-            columns.Add(new ColumnObject("1学年", grade1Actions));
-
-            for (var i = 2; i <= 4; i++)
-            {
-                var gradeActions = new[]
-                {
-                    new MessageAction($"IS{i}", $">> {command} IS{i}"),
-                    new MessageAction($"IT{i}", $">> {command} IT{i}"),
-                    new MessageAction($"IE{i}", $">> {command} IE{i}")
-                };
-                columns.Add(new ColumnObject($"{i}学年", gradeActions));
-            }
-
-            if (!useAs) return columns;
-            asFooter ??= new UriAction("押すなよ！！", "https://www.sendai-nct.ac.jp/");
-            var asActions = new[]
-            {
-                new MessageAction("AS4", $">> {command} AS4"),
-                new MessageAction("AS5", $">> {command} AS5"),
-                asFooter
-            };
-            columns.Add(new ColumnObject(asColumnText, asActions));
-
-            return columns;
-        }
     }
 }
